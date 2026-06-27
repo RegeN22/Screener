73d6385361b1466bd8b0eb5632e69a2d7a9f3c50
RegeN22 <michaelsh2482@gmail.com>
2026-06-27 13:26:17 +0300

    fix dateing in js

--- DownloadHandler.js
+++ DownloadHandler.js
@@ -1,6 +1,11 @@
 export const downloadScreenshot = (url) => {
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed (0 = January)
+  const year = date.getFullYear();
+
   chrome.downloads.download({
     url: url,
-    filename: `Screener-Screenshot-${Date.getMonth()}-${Date.getDate()}-${Date.getFullYear()}.png`
+    filename: `Screener-Screenshot-${day}-${month}-${year}.png`
   });
 }
